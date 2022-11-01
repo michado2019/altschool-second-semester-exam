a981801421eec909aea290b14bebab0bc1d3f5c0
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-01 13:37:22 +0100

    html example page edit to add heading to example image

--- src/components/pages/courses/html/htmlTopics/HtmlExample.jsx
+++ src/components/pages/courses/html/htmlTopics/HtmlExample.jsx
@@ -5,6 +5,7 @@ export default function HtmlExample() {
 
   return (
     <div className='htmlExample-wrapper'>
+     <h2>Code</h2>
      <img src='Screenshot (48).png' alt='exampleImg' className='htmlExample-img'/>
      <h2>Result</h2>
      <img src='Screenshot (49).png' alt='exampleResult-img' className='htmlExample-img'/>
