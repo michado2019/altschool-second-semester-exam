4c08be765c7dafac3b7e66bcff58b5fd4d2f2c43
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-02 17:42:42 +0100

    htmlintro example modified

--- src/components/pages/courses/html/htmlTopics/HtmlBasicExample.jsx
+++ src/components/pages/courses/html/htmlTopics/HtmlBasicExample.jsx
@@ -1,14 +1,16 @@
-import React from 'react'
-import './HtmlBasicExample.css'
-
+import React from "react";
+import "./HtmlBasicExample.css";
 export default function HtmlBasicExample() {
-
   return (
-    <div className='htmlExample-wrapper'>
-     <h2>Code</h2>
-     <img src='Screenshot (50).png' alt='exampleImg' className='htmlExample-img'/>
-     <h2>Result</h2>
-     <img src='Screenshot (51).png' alt='exampleResult-img' className='htmlExample-img'/>
+    <div className="htmlBasicExample-wrapper">
+      <a
+        href="https://www.w3schools.com/html/tryit.asp?filename=tryhtml_intro"
+        className="htmlBasicExample"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click me to view example!
+      </a>
     </div>
-  )
+  );
 }
--- src/components/pages/courses/html/htmlTopics/HtmlExample.jsx
+++ src/components/pages/courses/html/htmlTopics/HtmlExample.jsx
@@ -4,11 +4,15 @@ import './HtmlExample.css'
 export default function HtmlExample() {
 
   return (
-    <div className='htmlExample-wrapper'>
-     <h2>Code</h2>
-     <img src='Screenshot (48).png' alt='exampleImg' className='htmlExample-img'/>
-     <h2>Result</h2>
-     <img src='Screenshot (49).png' alt='exampleResult-img' className='htmlExample-img'/>
+    <div className="htmlExample-wrapper">
+      <a
+        href="https://www.w3schools.com/html/tryit.asp?filename=tryhtml_intro"
+        className="htmlExample"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Click me to view example!
+      </a>
     </div>
   )
 }
