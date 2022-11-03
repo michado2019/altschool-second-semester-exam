9338245f3eeaa3aac15a500d5efe72529950e9ac
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-03 13:53:28 +0100

    feat: SEO set for csspage

--- src/components/pages/courses/css/Css.jsx
+++ src/components/pages/courses/css/Css.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Css.css'
+import { Helmet } from 'react-helmet-async'
 
 export default function Css() {
   return (
     <div className='cssWrapper'>
+      <Helmet>
+        <title>CSS Tutorial</title>
+        <meta name="description" content="Learn CSS with easy" />
+        <link rel="canonical" href="/courses/css" />
+      </Helmet>
        <h1 className='cssCss'>Upcoming!</h1>
     </div>
   )
