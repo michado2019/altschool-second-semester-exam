9c7d481ebde6b26d34821a54980ff6cf5feb6052
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-03 14:32:56 +0100

    feat: SEO set for Javascript page

--- src/components/pages/courses/javascript/JavaScript.jsx
+++ src/components/pages/courses/javascript/JavaScript.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './JavaScript.css'
+import { Helmet } from 'react-helmet-async'
 
 export default function JavaScript() {
   return (
     <div className='javascriptWrapper'>
+      <Helmet>
+        <title>JAVASCRIPT Tutorial</title>
+        <meta name="description" content="Learn JAVASCRIPT with easy" />
+        <link rel="canonical" href="/courses/javascript" />
+      </Helmet>
        <h1 className='javascriptCss'>Upcoming!</h1>
     </div>
   )
