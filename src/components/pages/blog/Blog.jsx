57c9cbead29e2fa22b581c59d36438372c106c5e
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-03 13:48:02 +0100

    feat: SEO set for blogpage

--- src/components/pages/blog/Blog.jsx
+++ src/components/pages/blog/Blog.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Blog.css'
+import { Helmet } from 'react-helmet-async'
 
 export default function Blog() {
   return (
     <div className='blogWrapper'>
+      <Helmet>
+        <title>Blogpage</title>
+        <meta name="description" content="Welcome to my Blogpage" />
+        <link rel="canonical" href="/blog" />
+      </Helmet>
         <h1>Blog</h1>
         <div>
         Coming soonest!!!
