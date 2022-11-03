892007f9f3644b33abbab70118b8c29b46467015
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-03 14:44:41 +0100

    feat: SEO set for Sign and ErrorPage pages

--- src/components/pages/sign/Sign.jsx
+++ src/components/pages/sign/Sign.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 import './Sign.css'
+import { Helmet } from 'react-helmet-async'
 
 export default function Sign() {
   return (
     <div>
+      <Helmet>
+        <title>Sign in page</title>
+        <meta name="description" content="Sign in here" />
+        <link rel="canonical" href="/sign" />
+      </Helmet>
         Sign
     </div>
   )
