0daf143fb766fbfbd62762be893df6c4df3cf87d
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-05 18:13:42 +0100

    feat: The auth context used for authentication in the LayoutComponent

--- src/components/LayoutComponent.js
+++ src/components/LayoutComponent.js
@@ -1,12 +1,14 @@
-import React, { useState } from "react";
+import React, { useState, useContext, useEffect } from "react";
 import { NavLink, Link } from "react-router-dom";
 import "./LayoutComponent.css";
 import appLogo from "../images/logoImg.png";
+import { AuthContext } from "./context/AuthContext";
 import {
   DarkMode,
   LightMode,
   HomeOutlined,
   Login,
+  Logout,
   ExpandMore,
   ExpandLess,
   MenuOutlined,
@@ -14,9 +16,20 @@ import {
 } from "@mui/icons-material";
 
 export const Navbar = (props) => {
+  //use context
+  const { state, dispatch } = useContext(AuthContext);
+  const [userLoggedIn, setUserLoggedIn] = useState(true);
+
   //Set state for the dropdown menu
   const [expandCourses, setExpandCourses] = useState(false);
   const [menuContentsDisplay, setMenuContentsDisplay] = useState(false);
+
+  useEffect(() => {
+    setUserLoggedIn((prev) => !prev);
+    if (state === null) {
+      dispatch("LOGOUT");
+    }
+  }, [dispatch, state]);
 
   //Handle the dropdown menu
   const handleCoursesExpand = (e) => {
@@ -28,13 +41,18 @@ export const Navbar = (props) => {
   const handleDisplayMenuContents = (e) => {
     e.preventDefault();
     setMenuContentsDisplay((prev) => !prev);
+  };
+
+  //Handle signout
+  const handleSignout = (e) => {
+    e.preventDefault();
+    dispatch('LOGOUT')
   }
   const CustomNavbarLink = ({ to, ...props }) => {
     let activeStyle = {
       color: "red",
       transition: "all 0.3s",
     };
-
     return (
       <NavLink
         style={({ isActive }) =>
@@ -46,6 +64,7 @@ export const Navbar = (props) => {
       />
     );
   };
+
   //Styles
   const style = {
     display: expandCourses ? "block" : "none",
@@ -78,50 +97,77 @@ export const Navbar = (props) => {
               </h1>
               {expandCourses ? <ExpandLess /> : <ExpandMore />}
             </div>
-            <div id="coursesContents" style={style}>
-              <CustomNavbarLink
-                to="/courses/html"
-                className="layoutNavbar-links"
-                id="coursesLinks1"
-              >
-                <h1 className="coursesContents">HTML</h1>
-              </CustomNavbarLink>
-              <CustomNavbarLink
-                to="/courses/css"
-                className="layoutNavbar-links"
-                id="coursesLinks2"
-              >
-                <h1 className="coursesContents">CSS</h1>
-              </CustomNavbarLink>
-              <CustomNavbarLink
-                to="/courses/javascript"
-                className="layoutNavbar-links"
-                id="coursesLinks3"
-              >
-                <h1 className="coursesContents">JAVASCRIPT</h1>
-              </CustomNavbarLink>
-            </div>
+            {userLoggedIn ? (
+              <div style={style} className="layoutNavbar-links" id="loginFirst">
+                Login to view courses!
+              </div>
+            ) : (
+              <div id="coursesContents" style={style}>
+                <CustomNavbarLink
+                  to="/courses/html"
+                  className="layoutNavbar-links"
+                  id="coursesLinks1"
+                >
+                  <h1 className="coursesContents">HTML</h1>
+                </CustomNavbarLink>
+                <CustomNavbarLink
+                  to="/courses/css"
+                  className="layoutNavbar-links"
+                  id="coursesLinks2"
+                >
+                  <h1 className="coursesContents">CSS</h1>
+                </CustomNavbarLink>
+                <CustomNavbarLink
+                  to="/courses/javascript"
+                  className="layoutNavbar-links"
+                  id="coursesLinks3"
+                >
+                  <h1 className="coursesContents">JAVASCRIPT</h1>
+                </CustomNavbarLink>
+              </div>
+            )}
           </div>
-          <CustomNavbarLink to="/sign" className="layoutNavbar-links">
-            <Login id="login" />
-          </CustomNavbarLink>
-          {
-            menuContentsDisplay ? 
-          <CancelOutlined className="menuIcon" onClick={handleDisplayMenuContents}/> :
-          <MenuOutlined className="menuIcon" onClick={handleDisplayMenuContents} /> 
-          }
+          {userLoggedIn ? (
+            <CustomNavbarLink to="/sign" className="layoutNavbar-links">
+              <Login id="login" />
+            </CustomNavbarLink>
+          ) : (
+            <CustomNavbarLink className="layoutNavbar-links" id="logout">
+              <Logout />
+            </CustomNavbarLink>
+          )}
+          {menuContentsDisplay ? (
+            <CancelOutlined
+              className="menuIcon"
+              onClick={handleDisplayMenuContents}
+            />
+          ) : (
+            <MenuOutlined
+              className="menuIcon"
+              onClick={handleDisplayMenuContents}
+            />
+          )}
         </li>
       </ul>
-      <div className="displayControlled-navbar" style={{display: menuContentsDisplay ? 'flex' : 'none'}}>
+      <div
+        className="displayControlled-navbar"
+        style={{ display: menuContentsDisplay ? "flex" : "none" }}
+      >
         <Link to="/" className="link">
           <h1 className="displayControlled-navbar_text">Home</h1>
         </Link>
         <Link to="blog" className="link">
           <h1 className="displayControlled-navbar_text">Blog</h1>
         </Link>
-        <Link to="sign" className="link">
-          <h1 className="displayControlled-navbar_text">Sign in</h1>
-        </Link>
+        {userLoggedIn ? (
+          <Link to="sign" className="link">
+            <h1 className="displayControlled-navbar_text">Sign in</h1>
+          </Link>
+        ) : (
+          <Link>
+            <h1 className="displayControlled-navbar_text" onClick={handleSignout}>Sign out</h1>
+          </Link>
+        )}
       </div>
     </nav>
   );
