985cc1ac866a18b3ce22662e27dfa5a08c549bb9
Michael Adeshina <adeshinaobafemi09@gmail.com>
2022-11-05 18:04:39 +0100

    feat: Sign in page set for the app

--- src/components/pages/sign/Sign.jsx
+++ src/components/pages/sign/Sign.jsx
@@ -1,54 +1,102 @@
-import React from 'react'
-import './Sign.css'
-import { Helmet } from 'react-helmet-async'
+import React, { useContext, useEffect, useState } from "react";
+import "./Sign.css";
+import { Helmet } from "react-helmet-async";
+import useForm from "../../hooks/useForm";
+import { AuthContext } from "../../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function Sign() {
-  const name = 'Mike'
+  //Navigation
+  const navigate = useNavigate();
+
+  //Use context
+  const useUserContext = useContext(AuthContext);
+
+  //Set states
+  const [user, setUser] = useState({});
+  const { state, dispatch } = useUserContext;
+  const [userLoggedIn, setUserLoggedIn] = useState(state);
+
+  //useForm data
+  const { inputs, handleChange } = useForm({
+    firstName: "",
+    lastName: "",
+    email: "",
+  });
+
+  //Set useEffect for user
+  useEffect(() => {
+    setUser(inputs);
+  }, [inputs]);
+
+  //Handle submit event
+  const handleSubmit = (e) => {
+    if (!inputs.firstName || !inputs.lastName || !inputs.email) {
+      e.preventDefault();
+      alert("Please fill out all the fields");
+    }
+    e.preventDefault();
+    setUser(inputs);
+    setUserLoggedIn((prev) => {
+      return {
+        ...prev,
+        user,
+      };
+    });
+    if (userLoggedIn) {
+      dispatch("LOGIN");
+      navigate('/')
+    }
+  };
   return (
-    <div className='signWrapper'>
+    <div className="signWrapper">
       <Helmet>
         <title>Sign in page</title>
         <meta name="description" content="Sign in here" />
         <link rel="canonical" href="/sign" />
       </Helmet>
-        <form className='signForm'>
-        <label htmlFor="first_name" className='signForm-labels'>First name</label>
+      <form className="signForm" onSubmit={handleSubmit}>
+        <label htmlFor="first_name" className="signForm-labels">
+          First name
+        </label>
         <input
           type="text"
           placeholder="Enter your first name"
           className="signInput"
           id="first_name"
+          name="firstName"
+          value={inputs.firstName}
+          onChange={handleChange}
         />
-        <label htmlFor="Last_name" className='signForm-labels'>Last name</label>
+        <label htmlFor="Last_name" className="signForm-labels">
+          Last name
+        </label>
         <input
           type="text"
           placeholder="Enter your last name"
           className="signInput"
           id="last_name"
+          name="lastName"
+          value={inputs.lastName}
+          onChange={handleChange}
         />
-        <label htmlFor="email" className='signForm-labels'>Email</label>
+        <label htmlFor="email" className="signForm-labels">
+          Email
+        </label>
         <input
           type="text"
           placeholder="yourname@gmail.com"
           className="signInput"
           id="email"
+          name="email"
+          value={inputs.email}
+          onChange={handleChange}
           required
         />
-        <label htmlFor="message" className='signForm-labels'>Message</label>
-        <textarea
-          placeholder="Send me a message and I&#39;ll reply as soon as possible"
-          className="signInput"
-          id="message"
-        />
-        <input type="checkbox" id="checkbox" name="" value="" />
-        <label htmlFor="checkbox" className='signForm-labels' id='text_area'>
-          You agree to providing your data to <span id="name">{name}</span> who
-          may contact you.
-        </label>
         <button className="signInput" id="btn__submit">
           Sign in
         </button>
-        </form>
+      </form>
     </div>
-  )
+  );
 }
