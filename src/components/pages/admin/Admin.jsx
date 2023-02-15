5408c1d107dfdff62f699ec8d1a9cc8f22d01811
Michael Adeshina <adeshinaobafemi09@gmail.com>
2023-02-15 06:19:55 +0100

    feat: admin pass code modal added to the app

--- src/components/pages/admin/Admin.jsx
+++ src/components/pages/admin/Admin.jsx
@@ -1,10 +1,80 @@
-import React from 'react'
-import './Admin.css'
+import React, { useEffect, useState } from "react";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import Modal from "@mui/material/Modal";
+
+import "./Admin.css";
 
 function Admin() {
-  return (
-    <div className='adminWrapper'>Admin</div>
-  )
-}
+  // States
+  const [open, setOpen] = useState(false);
+  const [getPassword, setGetPassword] = useState('')
+  const [adminPassword, setAdminPassword] = useState('')
+  const [wrongPassword, setWrongPassword] = useState(false)
+  // Handlers
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleChange = (e) => {
+    setGetPassword(e.target.value)
+    setAdminPassword(getPassword)
+  }
+  const handlePasswordSubmit = (e) => {
+    e.preventDefault()
+      setAdminPassword(getPassword)
+      if(getPassword === '0987654567890' && getPassword !== ''){
+        handleClose()
+      }else{
+        setWrongPassword(true)
+      }
+  }
+  const style = {
+    marginTop: "300px",
+    transform: "translate(-50%, -50%)",
+    width: "50%",
+    bgcolor: "background.paper",
+    border: "2px solid #2f8aa5",
+    boxShadow: 24,
+    p: 4,
+    outline: "none",
+    marginLeft: "50%",
+    borderRadius: "10px",
+  };
 
-export default Admin
+  // useEffect
+  useEffect(() => {
+    handleOpen();
+  }, []);
+  return (
+    <div className="adminWrapper">
+      <Modal
+        open={open}
+        aria-labelledby="modal-modal-title"
+        aria-describedby="modal-modal-description"
+      >
+        <Box sx={style} className="modeBox">
+          <Typography id="modal-modal-title" variant="h6" component="h2">
+            <div className="modeBox-notifications_flex">
+            {
+              wrongPassword ?
+              <h2 className="modeBox-notifications">Enter valid admin password!</h2> :
+              <h2 className="modeBox-notifications">Enter admin password:</h2> 
+            }
+            </div>
+          </Typography>
+          <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+            <form onSubmit={handlePasswordSubmit}>
+            <input 
+            type="text/number" 
+            className="adminInput" 
+            onChange={handleChange}
+            />
+            <button className='adminSubmit-btn'>Submit</button>
+            </form>
+          </Typography>
+        </Box>
+      </Modal>
+      <h1>Admin</h1>
+    </div>
+  );
+}
+export default Admin;
